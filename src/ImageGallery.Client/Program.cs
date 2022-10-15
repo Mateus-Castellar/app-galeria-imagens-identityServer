d584e7ad3033c3b3be2ea32efdaf501654b5bf34
Mateus Castellar <mateuscastellar@outlook.com>
2022-10-15 17:54:27 -0300

    feat: Configurando app MVC para consuimr api e exibir imagens

--- src/ImageGallery.Client/Program.cs
+++ src/ImageGallery.Client/Program.cs
@@ -1,19 +1,27 @@
+using Microsoft.Net.Http.Headers;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddJsonOptions(configure =>
+    configure.JsonSerializerOptions.PropertyNamingPolicy = null); ;
+
+builder.Services.AddHttpClient("APIClient", client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["ImageGalleryAPIRoot"]);
+    client.DefaultRequestHeaders.Clear();
+    client.DefaultRequestHeaders.Add(HeaderNames.Accept, "application/json");
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() is false)
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler();
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
@@ -22,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Gallery}/{action=Index}/{id?}");
 
 app.Run();
---
+++ src/ImageGallery.Client/ViewModels/GalleryIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ImageGallery.Shared.Models;
+
+namespace ImageGallery.Client.ViewModels
+{
+    public class GalleryIndexViewModel
+    {
+        public IEnumerable<Image> Images { get; private set; }
+            = new List<Image>();
+
+        public GalleryIndexViewModel(IEnumerable<Image> images)
+        {
+            Images = images;
+        }
+    }
+}
