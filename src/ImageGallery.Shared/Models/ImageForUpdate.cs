85942157b4b7858d8f6ccb49e0ef6602e22b0189
Mateus Castellar <mateuscastellar@outlook.com>
2022-10-15 17:00:17 -0300

    feat: Models

---
+++ src/ImageGallery.Shared/Models/ImageForUpdate.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ImageGallery.Shared.Models
+{
+    public class ImageForUpdate
+    {
+        [Required]
+        [MaxLength(150)]
+        public string Title { get; set; }
+
+        public ImageForUpdate(string title)
+        {
+            Title = title;
+        }
+    }
+}
