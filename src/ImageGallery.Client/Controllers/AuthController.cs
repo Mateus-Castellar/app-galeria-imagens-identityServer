de401006c2b3fe9a97098fc69a052e2e01f26b12
Mateus Castellar <mateuscastellar@outlook.com>
2022-10-16 04:49:39 -0300

    feat: Logout na app Mvc (Client)

---
+++ src/ImageGallery.Client/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageGallery.Client.Controllers
+{
+    public class AuthController : Controller
+    {
+        [Authorize]
+        public async Task Logar()
+        {
+            await HttpContext.SignOutAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+
+    }
+}
